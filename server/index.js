ebfec226732aef327da82f0f9cc2ff21a3537245
Beata Tomczak <tma.btty@gmail.com>
2020-11-23 21:29:14 +0100

    Create post model, folder structure

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import cors from 'cors'
+
 import connectDB from './config/db.js'
+import postRoutes from './routes/posts.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
