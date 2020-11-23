db1f663252d1bd7b08c7e2c9ea4ccfbd9fcb2aa4
Beata Tomczak <tma.btty@gmail.com>
2020-11-23 21:06:25 +0100

    Connect to the database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
